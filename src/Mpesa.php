639dea429d6cb95f5cf30e45e7c1b2c815536d28
Karson <karson@turbohost.co>
2020-05-14 15:02:41 +0200

    implement guzzlehttp lib for request

--- src/Mpesa.php
+++ src/Mpesa.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace Karson\MpesaPhpSdk;
 
+use GuzzleHttp\Client;
+use function GuzzleHttp\json_decode;
 
-class Mpesa {
+class Mpesa
+{
 
-    private $api_url = 'https://api.sandbox.vm.co.mz';
-    private $api_port;
+    private $base_uri = 'https://api.sandbox.vm.co.mz';
     private $public_key;
     private $api_key;
 
@@ -18,17 +21,20 @@ class Mpesa {
         }
     }
 
-    public function setPublicKey($public_key){
+    public function setPublicKey($public_key)
+    {
         $this->public_key = trim($public_key);
     }
 
-    public function setApiKey($api_key){
+    public function setApiKey($api_key)
+    {
         $this->api_key = $api_key;
     }
 
-    public function setEnv($env){
+    public function setEnv($env)
+    {
         if ($env == 'live') {
-            $this->api_url = 'https://api.vm.co.mz';
+            $this->base_uri = 'https://api.vm.co.mz';
         }
     }
 
@@ -42,18 +48,18 @@ class Mpesa {
      * @param string $serviceCode Shortcode of the business where funds will be credited to.
      * @return \stdClass
      */
-    function c2b($transactionReference, $customerMSISDN, $amount, $thirdPartReferece, $serviceCode){
+    function c2b($transactionReference, $customerMSISDN, $amount, $thirdPartReferece, $serviceCode)
+    {
 
         $fields = [
-            "input_TransactionReference"=> $transactionReference,
-            "input_CustomerMSISDN"=> $customerMSISDN,
-            "input_Amount"=> $amount,
-            "input_ThirdPartyReference"=> $thirdPartReferece,
+            "input_TransactionReference" => $transactionReference,
+            "input_CustomerMSISDN" => $customerMSISDN,
+            "input_Amount" => $amount,
+            "input_ThirdPartyReference" => $thirdPartReferece,
             "input_ServiceProviderCode" => $serviceCode
         ];
 
         return $this->makeRequest(':18352/ipg/v1x/c2bPayment/singleStage/', 'POST', $fields);
-
     }
 
     /**
@@ -69,15 +75,13 @@ class Mpesa {
     {
         $fields = [
             "input_TransactionID" => $transactionID,
-             "input_SecurityCredential" => $securityCredential,
-             "input_InitiatorIdentifier" => $initiatorIdentifier,
-             "input_ThirdPartyReference" => $thirdPartyReference,
-             "input_ServiceProviderCode" => $serviceProviderCode,
-             "input_ReversalAmount" => $reversalAmount
+            "input_SecurityCredential" => $securityCredential,
+            "input_InitiatorIdentifier" => $initiatorIdentifier,
+            "input_ThirdPartyReference" => $thirdPartyReference,
+            "input_ServiceProviderCode" => $serviceProviderCode,
+            "input_ReversalAmount" => $reversalAmount
         ];
-         return $this->makeRequest(':18354/ipg/v1x/reversal/', 'POST', $fields);
-
-
+        return $this->makeRequest(':18354/ipg/v1x/reversal/', 'POST', $fields);
     }
 
     /**
@@ -90,15 +94,14 @@ class Mpesa {
     {
 
         $fields = [
-             'input_ThirdPartyReference' => $thirdPartyReference,
-             'input_QueryReference' => $queryReference,
-             'input_ServiceProviderCode' => $serviceProviderCode
+            'input_ThirdPartyReference' => $thirdPartyReference,
+            'input_QueryReference' => $queryReference,
+            'input_ServiceProviderCode' => $serviceProviderCode
         ];
 
 
 
         return $this->makeRequest(':18353/ipg/v1x/queryTransactionStatus/', 'GET', $fields);
-
     }
 
     /**
@@ -107,8 +110,7 @@ class Mpesa {
     public function getToken()
     {
 
-        if (!empty($this->public_key) && !empty($this->api_key))
-        {
+        if (!empty($this->public_key) && !empty($this->api_key)) {
             $key = "-----BEGIN PUBLIC KEY-----\n";
             $key .= wordwrap($this->public_key, 60, "\n", true);
             $key .= "\n-----END PUBLIC KEY-----";
@@ -126,60 +128,46 @@ class Mpesa {
      * @param array $fields
      * @return \stdClass
      */
-    private function makeRequest(string $url,string $method, array $fields = [])
+    private function makeRequest(string $url, string $method, array $fields = [])
     {
-        $url = $this->api_url.$url;
 
-        $ch = curl_init();
+        $client = new Client([
+            'base_uri' => $this->base_uri,
+            'timeout' => 90,
+        ]);
 
-        if($method == 'POST')
-        {
-            $fieldsString = json_encode($fields);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsString);
+        $options = ['http_errors' => false, 'headers' => $this->getHeaders()];
+        if ($method == 'POST') {
+            $options +=  ['json' => $fields];
+        } else {
+            $options += ['query' => $fields];
         }
-        else
-        {
-            $fieldsString = http_build_query($fields);
-            $url .= '?'.$fieldsString;
-        }
-        $header = $this->getHeader();
 
-
-        curl_setopt($ch, CURLOPT_POST, count($fields));
-        curl_setopt($ch, CURLOPT_PORT, $this->api_port);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 90);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-        $result = curl_exec($ch);
+        $response = $client->request($method, $url, $options);
 
         $return = new \stdClass();
-        $return->response = json_decode($result);
+        $return->response = json_decode($response->getBody());
 
-        if ($return->response == false)
-            $return->response = $result;
+        if ($return->response == false) {
+            $return->response = $response->getBody();
+        }
 
-        $return->status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
+
+        $return->status = $response->getStatusCode();
         return $return;
     }
 
     /**
      * @return array
      */
-    private function getHeader()
+    private function getHeaders()
     {
-        $header = [
-            'Content-Type: application/json',
-            'Authorization: Bearer ' . $this->getToken(),
-            'origin: developer.mpesa.vm.co.mz',
-            'Connection: keep-alive'
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Authorization' =>  'Bearer ' . $this->getToken(),
+            'origin' => 'developer.mpesa.vm.co.mz',
+            'Connection' => 'keep-alive'
         ];
-        return $header;
+        return $headers;
     }
-
-
 }
