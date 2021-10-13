b479efceaaf3cf19e60ab1d2d0a692672b67df94
Karson <karson@turbohost.co>
2021-10-13 11:01:48 +0200

    laravel compatiblility

---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MpesaPhpSdk\Providers;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+/**
+ * Service provider
+ */
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        $this->publishes([
+            __DIR__.'/../config/mpesa.php', config_path('mpesa.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/mpesa.php', 'mpesa'
+        );
+
+        $this->app->bind('\Karson\MpesaPhpSdk\Mpesa', function ($app) {
+            $mpesa =  new \Karson\MpesaPhpSdk\Mpesa();
+
+            $mpesa->setPublicKey(conig('mpesa.public_key'));
+$mpesa->setApiKey(conig('mpesa.api_key'));//test
+
+$mpesa->setEnv(conig('mpesa.env'));
+
+return $mpesa;
+        });
+    }
+}
---
+++ src/config/mpesa.php
@@ -0,0 +1,13 @@
+<?php
+
+
+return [
+
+'api_key' => env('MPESA_API_KEY'),
+
+'public_key' => env('MPESA_PUBLIC_KEY'),
+
+'env' => env('MPESA_ENV'),
+
+
+];
